88927f88308ccbf846d4a1a7ebc01b61fae0ac8c
Andrea Zemignani <andrea.zemignani@acsoftware.it>
2025-04-27 14:50:53 +0200

    add kafka management

--- src/main/java/com/green/energy/tracker/user_management/config/KafkaStreamsExceptionHandler.java
+++ src/main/java/com/green/energy/tracker/user_management/config/KafkaStreamsExceptionHandler.java
@@ -16,9 +16,9 @@ public class KafkaStreamsExceptionHandler {
     private String userEventsTopicDLQ;
     private final KafkaTemplate<String, String> kafkaTemplate;
 
-    public StreamsUncaughtExceptionHandler.StreamThreadExceptionResponse handleUncaught(Throwable e) {
-        log.error("Uncaught Exception in Stream Thread: {}", e.getMessage(), e);
-        kafkaTemplate.send(userEventsTopicDLQ,"Uncaught: " + e.getMessage() + " No record context");
+    public StreamsUncaughtExceptionHandler.StreamThreadExceptionResponse handleUncaught(Throwable throwable) {
+        log.error("Uncaught Exception user event stream", throwable);
+        kafkaTemplate.send(userEventsTopicDLQ,throwable.getMessage());
         return StreamsUncaughtExceptionHandler.StreamThreadExceptionResponse.REPLACE_THREAD;
     }
 
