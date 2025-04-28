c866307586e9b6256419d3f8eb6f18fd6eb2bdbf
Andrea Zemignani <andrea.zemignani@acsoftware.it>
2025-04-28 15:14:05 +0200

    add kafka management

--- src/main/java/com/green/energy/tracker/user_management/kafka/KafkaStreamsExceptionHandler.java
+++ src/main/java/com/green/energy/tracker/user_management/kafka/KafkaStreamsExceptionHandler.java
@@ -9,7 +9,6 @@ import org.apache.kafka.streams.errors.ProductionExceptionHandler;
 import org.apache.kafka.streams.processor.ProcessorContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 import java.util.*;
