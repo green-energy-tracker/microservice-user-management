3940ca19a8d0862a9106e74ff241d28ead2b80cc
Andrea Zemignani <andrea.zemignani@acsoftware.it>
2025-05-19 20:56:20 +0200

    user repository management

--- src/main/java/com/green/energy/tracker/user_management/config/SecurityConfig.java
+++ src/main/java/com/green/energy/tracker/user_management/config/SecurityConfig.java
@@ -15,7 +15,7 @@ public class SecurityConfig {
                         .requestMatchers(
                                 "/swagger-ui/**",
                                 "/v3/api-docs/**",
-                                "/swagger-ui.html"
+                                "/swagger-ui.html/**"
                         ).permitAll()
                         .anyRequest().authenticated()
                 );
